d71579042e7e51baa92dc7226ff2b66ee5136be6
LAmena-dev <amena2laremm@gmail.com>
2025-12-22 23:01:02 +0800

    0.0.2.2 - fixing functions and classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./styles.css";
 
+const dueDate = document.querySelector("#dueDate");
+dueDate.min = new Date().toISOString().split("T")[0];
+
 const modals = document.querySelectorAll("dialog");
+
+// For assigning all current and future buttons and event listeners
 const openButtons = document.querySelectorAll("[data-open-modal]");
 const closeButtons = document.querySelectorAll(".closeModal");
 
@@ -18,41 +23,97 @@ closeButtons.forEach((button) => {
   });
 });
 
+// For building card elements
+function elementBuilder(tag, cls, text) {
+  const el = document.createElement(tag);
+  if (cls) el.classList.add(cls);
+  if (text != null) {
+    el.textContent = text;
+  }
+
+  return el;
+}
+
+// Tab Creation
+const tabList = document.querySelector(".tabList");
+const tabs = [];
+
 class Tab {
   constructor(data) {
-    this.name = data.name;
+    this.tabID = data.tabID;
+    this.tabName = data.tabName;
   }
 }
 
 function addTabToList(name) {
-  tabs.push(new Tab({ name }));
+  const id = crypto.randomUUID();
+  tabs.push(new Tab({ tabID: id, tabName: name }));
 }
 
+// Tab form add button
 const addTab = document.querySelector(".addTab");
 addTab.addEventListener("click", (e) => {
   e.preventDefault();
   const tabName = document.querySelector("#tab").value;
 
   addTabToList(tabName);
+  console.log(tabs);
+
+  const newTab = tabs[tabs.length - 1];
+  tabLoader(newTab);
   document.querySelector("dialog[open]").close();
 });
 
-const dueDate = document.querySelector("#dueDate");
-dueDate.min = new Date().toISOString().split("T")[0];
+let tabLoader = (tab) => {
+  const tabEntry = elementBuilder("div", "tab", tab.tabName);
+  tabEntry.dataset.tabID = tab.tabID;
+
+  const tabRemoveBtn = elementBuilder("button", "tabRemoveBtn", "Remove");
+  tabRemoveBtn.addEventListener("click", () => {
+    tabEntry.remove();
+    const index = tabs.findIndex(
+      (tabToRemove) => (tabToRemove.tabID = tab.tabID)
+    );
+    if (index !== -1) tabs.splice(index, 1);
+  });
+  tabEntry.append(tabRemoveBtn);
+  tabList.append(tabEntry);
+};
+
+let tabListLoader = () => {
+  tabs.forEach((tabList) => {
+    tabLoader(tabList);
+  });
+};
+
+// Task creation
+const taskList = document.querySelector("main");
+const tasks = [];
 
 class Task {
   constructor(data) {
-    this.name = data.name;
-    this.dueDate = data.dueDate;
-    this.desc = data.desc;
-    this.priority = data.priority;
+    this.taskID = data.taskID;
+    this.taskName = data.taskName;
+    this.taskDueDate = data.taskDueDate;
+    this.taskDesc = data.taskDesc;
+    this.taskPriority = data.taskPriority;
   }
 }
 
 function addTaskToTab(name, date, desc, priority) {
-  tasks.push(new Task({ name, dueDate: date, desc, priority }));
+  const id = crypto.randomUUID();
+  return tasks.push(
+    new Task({
+      taskID: id,
+      taskName: name,
+      taskDueDate: date,
+      taskDesc: desc,
+      taskPriority: priority,
+    })
+  );
 }
 
+// Task form add button
 const addTask = document.querySelector(".addTask");
 addTask.addEventListener("click", (e) => {
   e.preventDefault();
@@ -64,8 +125,49 @@ addTask.addEventListener("click", (e) => {
   ).id;
 
   addTaskToTab(taskName, dueDate, taskDesc, taskPriority);
+  console.log(tasks);
+
+  const newTask = tasks[tasks.length - 1];
+  taskLoader(newTask);
   document.querySelector("dialog[open]").close();
 });
 
-const tabs = [];
-const tasks = [];
+let taskLoader = (task) => {
+  const card = elementBuilder("article", "card");
+  card.dataset.taskID = task.taskID;
+
+  const taskCheckbox = elementBuilder("input", "taskCheckbox");
+  taskCheckbox.type = "checkbox";
+  const taskName = elementBuilder("h2", "taskName", task.taskName);
+  const taskDesc = elementBuilder("div", "taskDesc", task.taskDesc);
+  const cardFooter = elementBuilder("div", "cardFooter");
+  const taskDueDate = elementBuilder("p", "taskDueDate", task.dueDate);
+  const taskPriority = elementBuilder(
+    "button",
+    "taskPriority",
+    task.taskPriority
+  );
+
+  const taskRemoveBtn = elementBuilder("button", "taskRemoveBtn", "Remove");
+  taskRemoveBtn.addEventListener("click", () => {
+    card.remove();
+    const index = tasks.findIndex(
+      (taskToRemove) => (taskToRemove.taskID = task.taskID)
+    );
+    if (index !== -1) tasks.splice(index, 1);
+  });
+
+  cardFooter.append(taskDueDate, taskPriority, taskRemoveBtn);
+  card.append(taskCheckbox, taskName, taskDesc, cardFooter);
+
+  taskList.insertBefore(card, taskList.firstChild);
+};
+
+let taskListLoader = () => {
+  tasks.forEach((taskList) => {
+    taskLoader(taskList);
+  });
+};
+
+tabListLoader();
+taskListLoader();
